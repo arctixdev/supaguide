d2562f0b3b7c65427b908ed2d465f01f1a569f78
Jonathan Bangert <jonathan@bangert.dk>
2024-10-24 19:52:29 -0600

    Add login credentials

--- src/app/[tenant]/Login.tsx
+++ src/app/[tenant]/Login.tsx
@@ -16,7 +16,9 @@ export const Login = ({ formType = "pw-login", tenant, tenantName }) => {
   const isPasswordLogin = formType === FORM_TYPES.PASSWORD_LOGIN;
   const isMagicLinkLogin = formType === FORM_TYPES.MAGIC_LINK;
 
-  const formAction = isPasswordLogin ? urlPath(`/auth/pw-login`, tenant) : urlPath(`/auth/magic-link`, tenant);
+  const formAction = isPasswordLogin
+    ? urlPath(`/auth/pw-login`, tenant)
+    : urlPath(`/auth/magic-link`, tenant);
 
   useEffect(() => {
     const {
@@ -36,136 +38,153 @@ export const Login = ({ formType = "pw-login", tenant, tenantName }) => {
   }, []);
 
   return (
-    <form
-      method="POST"
-      action={formAction}
-      onSubmit={(event) => {
-        isPasswordLogin && event.preventDefault();
+    <>
+      <form
+        method="POST"
+        action={formAction}
+        onSubmit={(event) => {
+          isPasswordLogin && event.preventDefault();
 
-        if (isPasswordLogin) {
-          supabase.auth
-            .signInWithPassword({
-              email: emailInputRef.current.value,
-              password: passwordInputRef.current.value,
-            })
-            .then((result) => {
-              !result.data?.user && alert("Could not sign in"); // new version
+          if (isPasswordLogin) {
+            supabase.auth
+              .signInWithPassword({
+                email: emailInputRef.current.value,
+                password: passwordInputRef.current.value,
+              })
+              .then((result) => {
+                !result.data?.user && alert("Could not sign in"); // new version
 
-              /** old version:
+                /** old version:
               if (result.data?.user) {
                 router.push("/tickets");
               } else {
                 alert("Could not sign in");
               }
               */
-            });
-        }
-      }}
-    >
-      {isPasswordRecovery && (
-        <input type="hidden" name="type" value="recovery" />
-      )}
+              });
+          }
+        }}
+      >
+        {isPasswordRecovery && (
+          <input type="hidden" name="type" value="recovery" />
+        )}
 
-      <article style={{ maxWidth: "480px", margin: "auto" }}>
-        <header>
-          {isPasswordRecovery && <strong>Request new password</strong>}
-          {!isPasswordRecovery && <strong>Login</strong>}
-          <div style={{ display: "block", fontSize: "0.7em" }}>
-            {tenantName}
-          </div>
-        </header>
+        <article style={{ maxWidth: "480px", margin: "auto" }}>
+          <header>
+            {isPasswordRecovery && <strong>Request new password</strong>}
+            {!isPasswordRecovery && <strong>Login</strong>}
+            <div style={{ display: "block", fontSize: "0.7em" }}>
+              {tenantName}
+            </div>
+          </header>
 
-        <fieldset>
-          <label htmlFor="email">
-            Email{" "}
-            <input
-              ref={emailInputRef}
-              type="email"
-              id="email"
-              name="email"
-              required
-            />
-          </label>
-
-          {isPasswordLogin && (
-            <label htmlFor="password" style={{ marginTop: "20px" }}>
-              Password{" "}
+          <fieldset>
+            <label htmlFor="email">
+              Email{" "}
               <input
-                ref={passwordInputRef}
-                type="password"
-                id="password"
-                name="password"
+                ref={emailInputRef}
+                type="email"
+                id="email"
+                name="email"
+                required
               />
             </label>
-          )}
-        </fieldset>
 
-        <button type="submit">
-          {isPasswordLogin && "Sign in with Password"}
-          {isPasswordRecovery && "Request new password"}
-          {isMagicLinkLogin && "Sign in with Magic Link"}
-        </button>
+            {isPasswordLogin && (
+              <label htmlFor="password" style={{ marginTop: "20px" }}>
+                Password{" "}
+                <input
+                  ref={passwordInputRef}
+                  type="password"
+                  id="password"
+                  name="password"
+                />
+              </label>
+            )}
+          </fieldset>
 
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            flexDirection: "column",
-            gap: "6px",
-            marginBottom: "20px",
-          }}
-        >
-          {!isPasswordLogin && (
-            <Link
-              role="button"
-              className="contrast"
-              href={{
-                pathname: urlPath("/", tenant),
-                query: { magicLink: "no" },
-              }}
-            >
-              Go to Password Login
-            </Link>
-          )}
-          {!isMagicLinkLogin && (
-            <Link
-              role="button"
-              className="contrast"
-              href={{
-                pathname: urlPath("/", tenant),
-                query: { magicLink: "yes" },
-              }}
-            >
-              Go to Magic Link Login
-            </Link>
-          )}
-        </div>
+          <button type="submit">
+            {isPasswordLogin && "Sign in with Password"}
+            {isPasswordRecovery && "Request new password"}
+            {isMagicLinkLogin && "Sign in with Magic Link"}
+          </button>
 
-        {!isPasswordRecovery && (
-          <Link
-            href={{
-              pathname: urlPath("/", tenant),
-              query: { passwordRecovery: "yes" },
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              flexDirection: "column",
+              gap: "6px",
+              marginBottom: "20px",
             }}
+          >
+            {!isPasswordLogin && (
+              <Link
+                role="button"
+                className="contrast"
+                href={{
+                  pathname: urlPath("/", tenant),
+                  query: { magicLink: "no" },
+                }}
+              >
+                Go to Password Login
+              </Link>
+            )}
+            {!isMagicLinkLogin && (
+              <Link
+                role="button"
+                className="contrast"
+                href={{
+                  pathname: urlPath("/", tenant),
+                  query: { magicLink: "yes" },
+                }}
+              >
+                Go to Magic Link Login
+              </Link>
+            )}
+          </div>
+
+          {!isPasswordRecovery && (
+            <Link
+              href={{
+                pathname: urlPath("/", tenant),
+                query: { passwordRecovery: "yes" },
+              }}
+              style={{
+                textAlign: "center",
+                display: "block",
+              }}
+            >
+              Go to Password Recovery
+            </Link>
+          )}
+          <Link
+            href={urlPath("/register", tenant)}
             style={{
               textAlign: "center",
               display: "block",
+              marginTop: "1em",
             }}
           >
-            Go to Password Recovery
+            Create account
           </Link>
-        )}
-        <Link 
-          href={urlPath("/register", tenant)}
-          style={{
-            textAlign: "center",
-            display: "block",
-            marginTop: "1em",
-          }}
-        >
-          Create account
-        </Link>
-      </article>
-    </form>
+        </article>
+      </form>
+      <h1>DEMO Login credentials</h1>
+      <p>
+        <strong>Email:</strong>
+        <br />
+        <strong>
+          <code>test@{tenant}-tickets.arctix.dev</code>
+        </strong>
+      </p>
+      <p>
+        <strong>Password:</strong>
+        <br />
+        <strong>
+          <code>supersecretpassword</code>
+        </strong>
+      </p>
+    </>
   );
 };
